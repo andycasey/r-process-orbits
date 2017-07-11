6ee34c2100845ef536a67f70e9eb58a4c4493055
Andy Casey <andycasey@gmail.com>
2017-07-12 09:34:54 +1000

    moving furnitute

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # Build documents
-cd $TRAVIS_BUILD_DIR/document
+cd $TRAVIS_BUILD_DIR/article
 make
 
 # Push to GitHub
@@ -9,7 +9,7 @@ if [ -n "$GITHUB_API_KEY" ]; then
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan $TRAVIS_BRANCH-pdf
   git rm -rf .
-  git add -f document/*.pdf
+  git add -f article/*.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "Compiling PDFs"
   git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 fi
